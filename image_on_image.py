11551d7f94b63ce6acc80b012146d0d66e9e34fa
Arslan <76843389+cyrkaade@users.noreply.github.com>
2022-06-22 10:52:36 +0600

    Update image_on_image.py

--- image_on_image.py
+++ image_on_image.py
@@ -1,6 +1,6 @@
 from PIL import Image
 
-first_image = Image.open("ВашПуть/benefit.png")
-second_image = Image.open("ВашПуть/106044_benefit.jpg")
+first_image = Image.open("YourPath/benefit.png")
+second_image = Image.open("YourPath/106044_benefit.jpg")
 first_image.paste(second_image, (0, 200))
 first_image.show()
